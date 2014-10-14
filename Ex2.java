2f5e3017ba79e19703d5fbe6f9c37169e1b24f1e
Shino Kayashima <s14003@std.it-college.ac.jp>
2014-10-14 13:32:12 +0900

    Ex2end

--- Ex2.java
+++ Ex2.java
@@ -2,31 +2,31 @@ import java.util.Scanner;
 import java.util.Random;
 
 abstract class Ex {
-	abstract void execute();
+  abstract void execute();
 }
 
 class Ex2_0 extends Ex{
-	void execute(){
-		int x;
-		int y;
+  void execute(){
+    int x;
+    int y;
     int z;
 
-		x = 63;
-		y = 18;
+    x = 63;
+    y = 18;
     z = 12;
 
-		System.out.println("xの値は" + x + "です。");
-		System.out.println("yの値は" + y + "です。");
+    System.out.println("xの値は" + x + "です。");
+    System.out.println("yの値は" + y + "です。");
     System.out.println("zの値は" + z + "です。");
-		System.out.println("合計は" + (x + y + z) + "です。");
-		System.out.println("平均は" + (x + y + z) / 3 + "です。");
-	}
+    System.out.println("合計は" + (x + y + z) + "です。");
+    System.out.println("平均は" + (x + y + z) / 3 + "です。");
+  }
 }
 
 public class Ex2 {
-	public static void main(String[] args){
-		Ex a;
-		a = new Ex2_0();
-		a.execute();
-	}
+  public static void main(String[] args){
+    Ex a;
+    a = new Ex2_0();
+    a.execute();
+  }
 }
